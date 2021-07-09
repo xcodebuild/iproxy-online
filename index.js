6d962307f2ac1998c5d2f807c895dea507f16c53
xcodebuild <me@xcodebuild.com>
2021-07-09 18:08:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
  */
 
 window.__ONLINE__ = {};
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154996514-2');
+
+
+(function(){
+  const tag = document.createElement('script');
+  tag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-154996514-2';
+  document.body.appendChild(tag);
+})()
