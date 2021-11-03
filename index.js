49a06694dbf26e773f15ab02379b7d4f47121bf7
shejinxin <shejinxin@corp.netease.com>
2021-11-03 14:12:07 +0800

    update gtag

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-154996514-2');
+  gtag('config', 'G-H3TWZC29B2');
 
 
 (function(){
   const tag = document.createElement('script');
-  tag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-154996514-2';
+  tag.src = 'https://www.googletagmanager.com/gtag/js?id=G-H3TWZC29B2';
   document.body.appendChild(tag);
 })()
